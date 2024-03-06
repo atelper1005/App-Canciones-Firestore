f2d31594b315c3f4009ab111359e78a1a1342df1
atelper1005 <atelper1005@g.educaand.es>
2024-03-06 10:36:38 +0100

    mapa visible pero no funcional

--- src/app/datos-app/datos-app.page.ts
+++ src/app/datos-app/datos-app.page.ts
@@ -8,9 +8,11 @@ import * as L from 'leaflet';
 })
 export class DatosAppPage implements OnInit {
 
-  map: any;
+  map: L.Map;
 
-  constructor() { }
+  constructor() {
+    this.map = {} as L.Map;
+   }
 
   ngOnInit() {
   }
@@ -23,14 +25,17 @@ export class DatosAppPage implements OnInit {
     let latitud = 36.6797047;
     let longitud = -5.4470656;
     let zoom = 17;
+//    this.map = L.map("mapId").setView([latitud, longitud], zoom).dragging.enable();
     this.map = L.map("mapId").setView([latitud, longitud], zoom);
+    this.map.dragging.enable();
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
         .addTo(this.map);
+    L.marker([latitud,longitud]).addTo(this.map);
 
-        const markPoint = L.marker([latitud, longitud]);
-        markPoint.bindPopup('<p>Ntra. Sra. de los Remedios - Ubrique.</p>');
-        this.map.addLayer(markPoint);
-        this.map.dragging.enable();
+        // var markPoint = L.marker([latitud, longitud]);
+        // markPoint.bindPopup('<p>Ntra. Sra. de los Remedios - Ubrique.</p>');
+        // this.map.addLayer(markPoint);
+        
       }
 
   }
